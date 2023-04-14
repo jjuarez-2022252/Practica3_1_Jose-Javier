2b4dc2cb6415ee784df0cc3da4f424345b4e57a8
jjuarez-2022252 <128552028+jjuarez-2022252@users.noreply.github.com>
2023-04-14 16:54:42 -0600

    Add files via upload

---
+++ Practica3_2_2/Practica3_2_2.ino
@@ -0,0 +1,136 @@
+/*
+   Fundacion Kinal
+   Centro educativo tecnico laboral Kinal
+   Quinto perito
+   Quinto electronica
+   Codigo Tecnico: EB5AM / EB5AV
+   Curso: Taller de electronica digital y reparacion de computadoras I
+   Proyecto: SECUENCIA DE LUCES AUTOMATICAS.
+   Nombre: Jose Javier Juarez Rivera
+   Encardo: Profe. Alejandro Lopez Navas
+   Fecha: 13 de Abril
+*/
+#define led0 2
+#define led1 3
+#define led2 4
+#define led3 5
+#define led4 6
+#define led5 7
+#define led6 8
+#define led7 9
+#define led8 10
+#define led9 11
+void setup()
+{
+ pinMode(led0,OUTPUT);
+ pinMode(led1,OUTPUT);
+ pinMode(led2,OUTPUT);
+ pinMode(led3,OUTPUT);
+ pinMode(led4,OUTPUT);
+ pinMode(led5,OUTPUT);
+ pinMode(led6,OUTPUT);
+ pinMode(led7,OUTPUT);
+ pinMode(led8,OUTPUT);
+ pinMode(led9,OUTPUT);
+ 
+
+}
+
+void loop()  
+{
+ digitalWrite(led0,HIGH);
+ delay(50);
+ digitalWrite(led0,LOW);
+ delay(10);
+ 
+ digitalWrite(led1,HIGH);
+ delay(50);
+ digitalWrite(led1,LOW);
+ delay(10);
+ 
+ digitalWrite(led2,HIGH);
+ delay(50);
+ digitalWrite(led2,LOW);
+ delay(10);
+ 
+ digitalWrite(led3,HIGH);
+ delay(50);
+ digitalWrite(led3,LOW);
+ delay(10);
+ 
+ digitalWrite(led4,HIGH);
+ delay(50);
+ digitalWrite(led4,LOW);
+ delay(10);
+ 
+ digitalWrite(led5,HIGH);
+ delay(50);
+ digitalWrite(led5,LOW);
+ delay(10);
+ 
+ digitalWrite(led6,HIGH);
+ delay(50);
+ digitalWrite(led6,LOW);
+ delay(10);
+ 
+ digitalWrite(led7,HIGH);
+ delay(50);
+ digitalWrite(led7,LOW);
+ delay(10);
+
+ digitalWrite(led8,HIGH);
+ delay(50);
+ digitalWrite(led8,LOW);
+ delay(10);
+ 
+ digitalWrite(led9,HIGH);
+ delay(50);
+ digitalWrite(led9,LOW);
+ delay(10);
+
+ digitalWrite(led8,HIGH);
+ delay(50);
+ digitalWrite(led8,LOW);
+ delay(10);
+ 
+ digitalWrite(led7,HIGH);
+ delay(50);
+ digitalWrite(led7,LOW);
+ delay(10);
+ 
+ digitalWrite(led6,HIGH);
+ delay(50);
+ digitalWrite(led6,LOW);
+ delay(10);
+ 
+ digitalWrite(led5,HIGH);
+ delay(50);
+ digitalWrite(led5,LOW);
+ delay(10);
+ 
+ digitalWrite(led4,HIGH);
+ delay(50);
+ digitalWrite(led4,LOW);
+ delay(10);
+ 
+ digitalWrite(led3,HIGH);
+ delay(50);
+ digitalWrite(led3,LOW);
+ delay(10);
+ 
+  digitalWrite(led2,HIGH);
+ delay(50);
+ digitalWrite(led2,LOW);
+ delay(10);
+ 
+ digitalWrite(led1,HIGH);
+ delay(50);
+ digitalWrite(led1,LOW);
+ delay(10);
+ 
+ digitalWrite(led0,HIGH);
+ delay(50);
+ digitalWrite(led0,LOW);
+ delay(10);
+ 
+}
