e2a9640a92f12e915f3f0d806376cc27579c18a3
jjuarez-2022252 <128552028+jjuarez-2022252@users.noreply.github.com>
2023-04-21 17:04:06 -0600

    Add files via upload

---
+++ Bomba/Bomba.ino
@@ -0,0 +1,24 @@
+/*
+Centro Educativo Tecnico Kinal
+Electronica
+Grado: Quinto
+Curso: Taller de Electronica Digital
+Blumno: Josué David Padilla Morataya
+Seccion: A
+Carne: 2022074
+*/
+#define Bomba 8
+#define sensor 9
+#define LdB 2
+#define LdR 4
+#define estado
+void setup() {
+pinMode(8,OUTPUT);
+pinMode(9,INPUT);
+pinMode(2,OUTPUT);
+pinMode(4,OUTPUT);
+}
+
+void loop() {
+
+}
